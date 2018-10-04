60020750a3d25b74bb5841fe05a959e51940af47
Ruchadapong <Production6115338@gmail.com>
2018-10-04 10:11:20 +0700

    4/10/61 10:11AM

--- basicLaravel/resources/views/Project/about.blade.php
+++ basicLaravel/resources/views/Project/about.blade.php
@@ -2,3 +2,61 @@
 
 @section('title',"Health Land | About")
 @section('nav-about','active')
+
+<div id="carouselExampleIndicators" class="carousel slide carousel-fade d-none d-md-block" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-none d-sm-block w-100 " src="img/slide-2.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-sm-block">
+        <h1>
+              <strong class="Welcome wow fadeInUp Slow">Welcome To Health Land</strong>
+            </h1>
+
+        <p>
+          <strong class="strong wow fadeInDown Slow">The Best Massage&Spa of The Year </strong>
+        </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-none d-sm-block w-100" src="img/slide-3.jpg" alt="Second slide">
+      <div class="carousel-caption d-none d-sm-block">
+        <h1>
+              <strong class="Welcome wow fadeInUp Slow">Welcome To Health Land</strong>
+            </h1>
+
+        <p>
+          <strong class="strong wow fadeInDown Slow">The Best Massage&Spa of The Year </strong>
+        </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-none d-sm-block w-100" src="img/slide-4.jpg" alt="Third slide">
+      <div class="carousel-caption d-none d-sm-block">
+        <h1>
+              <strong class="Welcome wow fadeInUp Slow">Welcome To Health Land</strong>
+            </h1>
+
+        <p>
+          <strong class="strong wow fadeInDown Slow">The Best Massage&Spa of The Year </strong>
+        </p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<div class="container About">
+  <section class=" wow fadeInLeft Slow BackgroundContent">
+    <p class="Text-White">
+      สปาหรูและผ่อนคลายให้บริการนวดหลากหลายรูปแบบในทำเลที่สะดวกสบายเราอยู่กึ่งกลางระหว่างเอ็มโพเรี่ยมและหมู่บ้านเคบนถนนสุขุมวิท 26 เพียง 800 เมตร จากสถานีรถไฟฟ้าพร้อมพงษ์
+    </p>
+
+  </section>
+</div>
