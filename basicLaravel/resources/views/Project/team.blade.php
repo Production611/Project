df0f0bc0b4c477076a8b15a81995768bcde73797
Ruchadapong <Production6115338@gmail.com>
2018-10-04 21:33:54 +0700

    Start Team Page 21.33 PM 4/10/61

--- basicLaravel/resources/views/Project/team.blade.php
+++ basicLaravel/resources/views/Project/team.blade.php
@@ -2,3 +2,52 @@
 
 @section('title',"Health Land | Team")
 @section('nav-team','active')
+
+<div id="carouselExampleIndicators" class="carousel slide carousel-fade d-none d-md-block" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-none d-sm-block w-100 " src="img/slide-5.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-sm-block">
+        <h1>
+          <strong class="Welcome wow fadeInUp Slow">Welcome To Health Land</strong>
+        </h1>
+
+        <p>
+          <strong class="strong wow fadeInDown Slow">The Best Massage&Spa of The Year </strong>
+        </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-none d-sm-block w-100" src="img/slide-6.jpg" alt="Second slide">
+      <div class="carousel-caption d-none d-sm-block">
+        <h1>
+          <strong class="Welcome wow fadeInUp Slow">Welcome To Health Land</strong>
+        </h1>
+
+        <p>
+          <strong class="strong wow fadeInDown Slow">The Best Massage&Spa of The Year </strong>
+        </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-none d-sm-block w-100" src="img/slide-7.jpg" alt="Third slide">
+      <div class="carousel-caption d-none d-sm-block">
+        <h1>
+          <strong class="Welcome wow fadeInUp Slow">Welcome To Health Land</strong>
+        </h1>
+
+        <p>
+          <strong class="strong wow fadeInDown Slow">The Best Massage&Spa of The Year </strong>
+        </p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
